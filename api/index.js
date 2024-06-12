c26db2ae0e3f332e2b73b2637bbfa3285d5c68a3
Chittireddy Akhil Reddy <akhil.ch.reddy@gmail.com>
2024-06-12 14:37:30 +0530

    second commit

--- api/index.js
+++ api/index.js
@@ -6,22 +6,22 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import path from 'path';
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-
-app.use(cookieParser());
-
-const __dirname = path.resolve();
-
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
@@ -32,11 +32,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
